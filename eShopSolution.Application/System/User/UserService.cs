6055d4fcf8b95ddbbbef5b504f5aabc5ab9cfbdd
Quan Nguyen <quannm2812@gmail.com>
2021-07-28 13:20:23 +0700

    login

---
+++ eShopSolution.Application/System/User/UserService.cs
@@ -0,0 +1,74 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.ViewMoldes.System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.User
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManage;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IConfiguration _config;
+        public UserService(UserManager<AppUser> userManage,SignInManager<AppUser> signInManage, RoleManager<AppRole> roleManager, IConfiguration config)
+        {
+            _userManager = userManage;
+            _signInManage = signInManage;
+            _roleManager = roleManager;
+            _config = config;
+
+        }
+        public async Task<string> Authencate(LoginRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null) return null;
+            var result = await _signInManage.PasswordSignInAsync(user, request.PassWord, request.RememberMe, true);
+            if (!result.Succeeded)
+            {
+                return null;
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var claim = new[]
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Role,string.Join(",",roles)),
+             };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Token:Iusaer"],
+                _config["Token:Iusaer"], 
+                claim,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new AppUser()
+            {
+                Dob=request.Dob,
+                Email=request.Email,
+                FirstName=request.FirstName,
+                LastName=request.LastName,
+                UserName=request.UserName,
+                PhoneNumber=request.PhoneNumber,
+            };
+            var result = await _userManager.CreateAsync(user, request.PassWord);
+            if (result.Succeeded)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ eShopSolution.BackendAPI/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using eShopSolution.Application.System.User;
+using eShopSolution.ViewMoldes.System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _usersService;
+        public UsersController(IUserService usersService)
+        {
+            _usersService = usersService;
+        }
+        [HttpPost("authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var resultToken = await _usersService.Authencate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest(" Incorrect");
+            }
+            return Ok(new { token = resultToken });
+        }
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _usersService.Register(request);
+            if (!result)
+            {
+                return BadRequest(" Register is Unsuccessful.");
+            }
+            return Ok();
+        }
+    }
+}
